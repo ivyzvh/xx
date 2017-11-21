32cac0c0479d94c38931ccb443e28cca5c0e537b
ivyzvh <ivyzvh@qq.com>
2017-11-21 13:54:36 +0800

    第一次提交，测试

---
+++ src/com/rene/Test.java
@@ -0,0 +1,21 @@
+package com.rene;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("AZ LIGHTING".compareTo("ARTELUM"));
+		System.out.println("b".compareTo("a"));
+		
+		if ("b".compareTo("a") > 0) {
+			// b加在a后
+		} else {
+			// 加在前面
+		}
+		
+		
+		
+		
+	}
+
+}
