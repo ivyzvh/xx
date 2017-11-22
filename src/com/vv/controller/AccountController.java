6786c3824e9786015dbfae50d622d7cdb97a0265
ivyzvh <ivyzvh@qq.com>
2017-11-22 10:57:34 +0800

    冲突修改

--- src/com/vv/controller/AccountController.java
+++ src/com/vv/controller/AccountController.java
@@ -1,11 +1,14 @@
 package com.vv.controller;
 
-import java.util.List;
-
 public class AccountController {
-
+	
+	
+	public String getAccountHome() {
+		return "/account/home";
+	}
+	
 	public List<String> getAccountList() {
 		return null;
 	}
-	
+
 }
