b40542d73d17212e96459e5c0541af2c11bf584d
ivyzvh <ivyzvh@qq.com>
2017-11-23 14:09:37 +0800

    Test develop分支

---
+++ src/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		out("develop");
+	}
+	
+	public static void out(Object o) {
+		System.out.println(o);
+	}
+	
+	public void out() {
+		System.out.println("dev");
+	}
+	
+}
