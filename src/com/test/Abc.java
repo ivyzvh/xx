13d729a3a7ff26501c4b1afa8af541b8c4e0a601
ivyzvh <ivyzvh@qq.com>
2017-11-23 16:31:07 +0800

    xxxx

--- src/com/test/Abc.java
+++ src/com/test/Abc.java
@@ -2,9 +2,18 @@ package com.test;
 
 public class Abc {
 
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
