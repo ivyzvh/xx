7d6f5417d495d4bddde3fc28a74d787962993c6f
ivyzvh <ivyzvh@qq.com>
2017-11-23 14:44:06 +0800

    添加Country

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,11 +3,20 @@ package com.test;
 public class Test {
 
 	public static void main(String[] args) {
-		out("master");
+		out("master-rene");
 	}
 	
 	public static void out(Object s) {
 		System.out.println(s);
+	}
+	
+	public double getSalary() {
+		return 1000;
+	}
+	
+	public void addAge(int age) {
+		int x = age;
+		System.out.println(x);
 	}
 
 }
