be8cfd0a3b844fee6bfe38cf0f824e370386eb3d
ivyzvh <ivyzvh@qq.com>
2017-11-23 14:12:18 +0800

    添加Test.java

---
+++ src/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		out("master");
+	}
+	
+	public static void out(Object s) {
+		System.out.println(s);
+	}
+
+}
