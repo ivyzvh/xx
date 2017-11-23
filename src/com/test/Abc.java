832ee467594efe2710bc9126cd81b7e05a0b8f3a
ivyzvh <ivyzvh@qq.com>
2017-11-23 16:33:01 +0800

    xxxx

--- src/com/test/Abc.java
+++ src/com/test/Abc.java
@@ -1,8 +1,8 @@
 package com.test;
 
 public class Abc {
-
-<<<<<<< HEAD
+	private String name;
+	private int age;
 	private String address;
 
 	public String getAddress() {
@@ -11,15 +11,6 @@ public class Abc {
 
 	public void setAddress(String address) {
 		this.address = address;
-	}
-
-=======
-	private String name;
-	private int age;
-	
->>>>>>> branch 'master' of https://github.com/ivyzvh/xx.git
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	}
 
 	public String getName() {
@@ -37,7 +28,9 @@ public class Abc {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
 
 }
